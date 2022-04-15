ceefc6962a237fb731be32cc3c0a34e5a5df83a7
GitHub Action <action@github.com>
2022-04-15 17:53:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/antoniohbmonteiro/testingplugintemplate/MyBundle.kt
+++ src/main/kotlin/com/github/antoniohbmonteiro/testingplugintemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.antoniohbmonteiro.testingplugintemplate
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/antoniohbmonteiro/testingplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.antoniohbmonteiro.testingplugintemplate.services
+
+import com.github.antoniohbmonteiro.testingplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
